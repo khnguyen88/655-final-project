deaff18e7e82d561febc7d88659f43e3292efe60
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-03-25 00:41:22 -0400

    Fleshed out image service a bit

--- 655-final-project-frontend/src/app/service/images.service.ts
+++ 655-final-project-frontend/src/app/service/images.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../private/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImagesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+  }
+
+  getSearchResultsHistory(): Observable<any>{
+    return this.http.get(environment.getSearchResultsHistoryPathName);
+  }
 }
