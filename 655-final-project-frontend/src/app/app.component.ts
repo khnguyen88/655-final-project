1fd26c6aed6f5bb8d1f5b19dedf9f52327c7a724
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-04-06 00:18:28 -0400

    Created the initial application using primeng components

--- 655-final-project-frontend/src/app/app.component.ts
+++ 655-final-project-frontend/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ResultsHistoryGalleryComponent } from './component/results-history-gallery/results-history-gallery.component';
+import { FileUploadFormComponent } from './component/file-upload-form/file-upload-form.component';
+import { TabsModule } from 'primeng/tabs';
+import { FileUploadModule } from 'primeng/fileupload';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ResultsHistoryGalleryComponent, FileUploadFormComponent, TabsModule, FileUploadModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
