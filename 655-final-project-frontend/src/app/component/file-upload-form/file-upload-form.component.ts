b9ef36572e30625b36b12dc7347889e7e25ffa53
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-04-06 00:18:04 -0400

    Created the file-upload-form component using prime-ng fileupload component. Though its not really working right now. Womp womp.

--- 655-final-project-frontend/src/app/component/file-upload-form/file-upload-form.component.ts
+++ 655-final-project-frontend/src/app/component/file-upload-form/file-upload-form.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { FileUpload, FileUploadEvent } from 'primeng/fileupload';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+
+interface UploadEvent {
+    originalEvent: Event;
+    files: File[];
+}
 
 @Component({
   selector: 'app-file-upload-form',
-  imports: [],
+  imports: [FileUpload, ToastModule, ButtonModule],
+  providers: [MessageService],
   templateUrl: './file-upload-form.component.html',
   styleUrl: './file-upload-form.component.scss'
 })
-export class FileUploadFormComponent {
 
+export class FileUploadFormComponent {
+  urlPath: string = 'https://final-project-655-put-image-into-storage-bucket-770833528905.us-central1.run.app'
+  
+  constructor(private messageService: MessageService) {}
+
+  onUpload(event: FileUploadEvent) {
+    alert(JSON.stringify(event));
+    this.messageService.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded with Basic Mode' });
+  }
 }
