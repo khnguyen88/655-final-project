d64fa3e21386447c73f957afe117b1450414b3ef
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2025-04-06 00:16:41 -0400

    Created the image service and results-history-gallery component. Handles retrieving json file that contains link to image url and api response results. Use it to populat the gallery page. Note: cors configuration needs to be apply for google storage bucket!!

--- 655-final-project-frontend/src/app/component/results-history-gallery/results-history-gallery.component.ts
+++ 655-final-project-frontend/src/app/component/results-history-gallery/results-history-gallery.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy, OnInit } from '@angular/core';
+import { ImagesService } from '../../service/images.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-results-history-gallery',
-  imports: [],
   templateUrl: './results-history-gallery.component.html',
-  styleUrl: './results-history-gallery.component.scss'
+  styleUrl: './results-history-gallery.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [],
 })
-export class ResultsHistoryGalleryComponent {
+  
+export class ResultsHistoryGalleryComponent implements OnInit, OnDestroy{
+  subscriptions: Subscription = new Subscription();
+
+  imageRequestHistoryData: any;
+
+  constructor(private imageService: ImagesService, private router: Router, private cd: ChangeDetectorRef) {
+    
+  }
+
+  ngOnInit(): void {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+
+    this.subscriptions.add(this.imageService.getSearchResultsHistory().subscribe(
+      results => {
+        this.imageRequestHistoryData = results;
+        alert(JSON.stringify(this.imageRequestHistoryData));
+        this.cd.detectChanges();
+      }
+    ));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
 }
--- 655-final-project-frontend/src/app/service/images.service.ts
+++ 655-final-project-frontend/src/app/service/images.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from '../../../private/environment';
 
 @Injectable({
@@ -12,6 +12,24 @@ export class ImagesService {
   }
 
   getSearchResultsHistory(): Observable<any>{
-    return this.http.get(environment.getSearchResultsHistoryPathName);
+    return this.http.get(environment.getSearchResultsHistoryPathName)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  // Error Handling Source: https://v17.angular.io/guide/http-handle-request-errors
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
